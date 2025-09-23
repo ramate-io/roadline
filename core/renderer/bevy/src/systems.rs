9cc3409c362668c5ddc86bc9b4c17517693e4396
Liam Makena Monninger <liam@ramate.io>
2025-09-23 13:48:01 +0100

    chore: remove click interaction system as it doesn't yet work.

--- core/renderer/bevy/src/systems.rs
+++ core/renderer/bevy/src/systems.rs
@@ -1,9 +1,7 @@
-pub mod click_selection;
 pub mod dependency;
 pub mod task;
 pub mod task_cursor_interaction;
 
-pub use click_selection::click_selection_system;
 pub use dependency::DependencySystemConfig;
 pub use task::TaskSystemConfig;
 pub use task_cursor_interaction::task_cursor_interaction_system;
