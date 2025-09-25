9e68403e7358efed2a3d827bb1aa27aa5531ef58
Liam Makena Monninger <liam@ramate.io>
2025-09-25 11:13:48 +0100

    chore: add comments about relevant events.

---
+++ core/renderer/bevy/src/events/interactions/output/task/task_selection_changed.rs
@@ -0,0 +1,2 @@
+// ! This module is the basic definition of the TaskSelectionChangedEvent.
+// ! It contains the core structs and helper functions for the event.
---
+++ core/renderer/bevy/src/systems/task/cursor_interaction/clicks/events/output/task_selection_changed.rs
@@ -0,0 +1,3 @@
+// ! This module is the implementation of the system which emits the TaskSelectionChangedEvent.
+// ! It contains the core structs and helper functions for the event.
+// ! It is buildiable as an independent system and via it's helper functions can be easily rolled into other systems.
