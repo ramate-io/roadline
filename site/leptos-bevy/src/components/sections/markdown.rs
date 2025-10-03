53bc009b02532dfd0166025fdde1785492ada3a0
Liam Makena Monninger <liam@ramate.io>
2025-10-03 15:34:57 +0100

    chore: add anchor links.

--- site/leptos-bevy/src/components/sections/markdown.rs
+++ site/leptos-bevy/src/components/sections/markdown.rs
@@ -1,5 +1,27 @@
-use comrak::{markdown_to_html, ComrakOptions};
+use comrak::{
+	adapters::{HeadingAdapter, HeadingMeta},
+	markdown_to_html_with_plugins,
+	nodes::Sourcepos,
+	ComrakOptions, Plugins,
+};
 use leptos::prelude::*;
+
+/*pub struct HeadingAdapterImpl;
+
+impl HeadingAdapter for HeadingAdapterImpl {
+	// Required methods
+	fn enter(
+		&self,
+		output: &mut dyn core::fmt::Write,
+		heading: &HeadingMeta,
+		sourcepos: Option<Sourcepos>,
+	) -> core::fmt::Result {
+		Ok(())
+	}
+	fn exit(&self, output: &mut dyn core::fmt::Write, heading: &HeadingMeta) -> core::fmt::Result {
+		Ok(())
+	}
+}*/
 
 /// A section component that renders markdown content
 #[component]
@@ -7,7 +29,22 @@ pub fn MarkdownSection(
 	#[prop(into)] content: String,
 	#[prop(optional)] class: Option<&'static str>,
 ) -> impl IntoView {
-	let content = markdown_to_html(&content, &ComrakOptions::default());
+	let mut options = ComrakOptions::default();
+	options.extension.strikethrough = true;
+	options.extension.tagfilter = true;
+	options.extension.table = true;
+	options.extension.autolink = true;
+	options.extension.strikethrough = true;
+	options.extension.tagfilter = true;
+	options.extension.tasklist = true;
+	options.extension.superscript = true;
+	options.extension.subscript = true;
+	options.extension.footnotes = true;
+	options.extension.math_dollars = true;
+	options.extension.math_code = true;
+	options.extension.header_ids = Some("".to_string());
+
+	let content = markdown_to_html_with_plugins(&content, &options, &Plugins::default());
 
 	view! {
 		<section
